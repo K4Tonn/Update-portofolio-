685e4d776ea2d2e0e7e071b0c82dfce37df199be
K4Tonn <162270955+K4Tonn@users.noreply.github.com>
2025-01-16 00:15:51 +0700

    script.js

--- File nya/script.js
+++ File nya/script.js
@@ -41,4 +41,26 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Initial check to show elements already in view on page load
     handleScrollAnimation();
+
+    // Event listener for project cards on click for mobile devices
+    const projectCards = document.querySelectorAll(".project-cards .card");
+
+    projectCards.forEach(card => {
+        card.addEventListener("click", () => {
+            const description = card.querySelector(".description");
+            const isVisible = card.classList.contains("show-description");
+            if (!isVisible) {
+                // Show description
+                card.classList.add("show-description");
+                card.classList.remove("hide-description");
+            } else {
+                // Hide description
+                card.classList.add("hide-description");
+                setTimeout(() => {
+                    card.classList.remove("show-description");
+                    card.classList.remove("hide-description");
+                }, 500); // Delay to match transition
+            }
+        });
+    });
 });
